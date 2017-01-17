6f00afcc34e084a104de2e896ead68d2e9b5e598
Malgorzata Ostrowska <ostrowska.malgorzata.ewa@gmail.com>
2017-01-17 12:25:07 +0100

    Getter for Cart

--- projekt.php
+++ projekt.php
@@ -5,12 +5,29 @@ include_once 'library.php';
 
 
 $czekolada = new Product(1, 2,'czekolada',5);
+echo '<br>Product Czekolada: <br>';
 echo $czekolada -> getId() . ' ';
 echo $czekolada -> getPrice() . ' ';
 echo $czekolada ->getName() . ' ';
 echo $czekolada ->getQuantity() . '<br>';
 
 $gosia = new Customer(1,'Malgorzata', 'Ostrowska');
+echo '<br>Customer Gosia: <br>';
 echo $gosia -> getId() . ' ';
 echo $gosia -> getFirstName() . ' ';
-echo $gosia -> getLastName() . '<br>';;
+echo $gosia -> getLastName() . '<br>';
+
+
+$castomer1 = new Customer(1, 'Tomek', 'KKK');
+echo '<br>Customer Tomek: <br>';
+new Cart($castomer1);
+echo $castomer1->getId() . ' ';
+echo $castomer1->getFirstName() . ' ';
+echo $castomer1->getLastName() . '<br>';
+
+//new Cart('Test');  // mie może być string tylko obiekt klasy Customer inaczej błąd !!!!!!!!!
+
+$cart = new Cart($castomer1);
+echo '<br>First name from Cart($castomer1): <br>';
+echo $cart ->getCustomer()->getFirstName();
+
--- src/Cart.php
+++ src/Cart.php
@@ -1,8 +1,14 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+class Cart{
+    private $customer;
+    
+    public function __construct(Customer $customer) {
+        $this->customer =$customer;
+    }
+    
+    public function getCustomer()
+    {
+        return $this-> customer;
+    }
+}
